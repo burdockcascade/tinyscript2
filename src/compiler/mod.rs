aa7203240971a8fbd7d339431291c6d1206ad3b0
Peter Richmond <peter.richmond@outlook.com>
2023-05-23 19:52:15 +0100

    update class and function compiler

--- src/compiler/mod.rs
+++ src/compiler/mod.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 use std::fs;
-use log::{debug, error, info, trace};
+use log::{debug, trace};
 
 use crate::compiler::function::Function;
 use crate::compiler::token::Token;
@@ -11,18 +11,20 @@ mod frontend;
 mod function;
 mod token;
 
+const CLASS_CONSTRUCTOR_FUNCTION_NAME: &str = "constructor";
+
 // Compiler
 pub struct Compiler {
-    functions: Vec<Function>,
-    global_lookup: HashMap<String, usize>,
+    globals: HashMap<String, Value>,
+    global_lookup: HashMap<String, usize>
 }
 
 impl Compiler {
 
     pub fn new() -> Self {
         Compiler {
-            functions: vec![],
-            global_lookup: Default::default(),
+            globals: Default::default(),
+            global_lookup: Default::default()
         }
     }
 
@@ -40,46 +42,37 @@ impl Compiler {
             match token {
                 Token::Import(file) => {
                     debug!("Importing {}", file);
-                    let imported_script = fs::read_to_string(file).expect("Unable to read file");
+                    // let imported_script = fs::read_to_string(file).expect("Unable to read file");
                     // let script: Vec<Token> = frontend::parser::script(&imported_script).map_err(|e| e.to_string()).expect("err");
                 },
                 _ => {}
             }
         }
 
-        // now loop through the classes of the script
-        debug!("Compiling high level items");
+        let mut functions = Vec::new();
+
         for token in script.iter() {
             match token {
                 Token::Class(class_name, items) => {
 
-                    let mut has_constructor = false;
-
                     // create a new object for the class
                     let mut object = HashMap::new();
+                    let mut class_fields = vec![];
 
                     // loop
-                    for item in items {
+                    for item in items.iter() {
                         match item {
 
                             // add the function to the class
-                            Token::Function(func_name, _params, _statements) => {
-
-                                if func_name == "constructor" {
-                                    // if the class has a constructor, set the flag
-                                    has_constructor = true;
-                                }
-
-                                // create a new name for the function
-                                let new_name = format!("{}.{}", class_name.to_string(), func_name);
-
-                                // insert into object the name of the function with the new name in a FunctionRef
-                                object.insert(func_name.to_string(), Value::FunctionRef(new_name.clone()));
-
+                            Token::Function(func_name, params, statements) => {
+                                let func = Function::new(class_name, func_name, params.clone(), statements.clone());
+                                object.insert(func_name.to_string(), Value::FunctionRef(func.get_full_name().clone()));
+                                functions.push(func);
                             },
 
                             // add the variable to the class
-                            Token::Variable(name, value) => {
+                            Token::Variable(name, ..) => {
+                                class_fields.push(item.clone());
                                 object.insert(name.to_string(), Value::Null);
                             },
 
@@ -87,70 +80,36 @@ impl Compiler {
                         }
                     }
 
-                    // add the constructor if it doesn't exist
-                    if !has_constructor {
-                        // create a new name for the function
-                        let new_name = format!("{}.{}", class_name.to_string(), "constructor");
+                    // add the default constructor if it doesn't exist
+                    if object.contains_key(CLASS_CONSTRUCTOR_FUNCTION_NAME) {
 
-                        // insert into object the name of the function with the new name in a FunctionRef
-                        object.insert("constructor".to_string(), Value::FunctionRef(new_name.clone()));
+                    } else {
+                        let default_constructor = Function::new(class_name, CLASS_CONSTRUCTOR_FUNCTION_NAME, Default::default(), class_fields);
+                        let fname = default_constructor.get_full_name().clone();
+                        functions.push(default_constructor);
+                        object.insert(CLASS_CONSTRUCTOR_FUNCTION_NAME.to_string(), Value::FunctionRef(fname));
                     }
 
                     // log class name and object
                     trace!("storing class {:?} with object '{:?}'", class_name.to_string(), object);
 
                     // insert the class into the globals
-                    let global_index = p.insert_global(Value::Class(object));
+                    let v = Value::Class(object);
+                    let global_index = p.insert_global(v.clone());
                     self.global_lookup.insert(class_name.to_string(), global_index);
+                    self.globals.insert(class_name.to_string(), v.clone());
 
                 },
                 _ => {}
             }
         }
 
-        // now loop through the functions of the script
-        debug!("Compiling functions");
-        for token in script.iter() {
-            match token {
-                Token::Class(class_name, items) => {
-
-                    for item in items {
-                        match item {
-
-                            // add the function to the class
-                            Token::Function(func_name, params, statements) => {
-
-                                let new_name = format!("{}.{}", class_name, func_name);
-
-                                // add the 'this' parameter to the function
-                                let mut new_params = params.to_vec();
-                                new_params.insert(0,Token::Identifier(String::from("this")));
-
-                                // create a new function with the new name
-                                let func = Function::new(class_name.as_str(), &new_name, new_params.as_slice(), statements.as_slice(), self.global_lookup.clone());
-                                self.functions.push(func);
-                            },
-                            _ => {}
-                        }
-                    }
-                }
-                _ => {}
-            }
-        }
-
-        // Compile function instructions into one program
-        debug!("Compiling program");
-        for func in self.functions {
-
-            // insert the function into the program
-            trace!("compiling function {} ", func.get_name());
-
-            // add the function to the program
-            p.symbols.insert(func.get_name().clone(), p.instructions.len());
-
-            // add the instructions of the function to the program
-            p.instructions.extend(func.get_instructions().clone());
-
+        for func in functions {
+            let fname = func.get_full_name().clone();
+            debug!("Compiling function {}", fname);
+            let ins = func.compile(self.globals.clone(), self.global_lookup.clone());
+            p.symbols.insert(fname, p.instructions.len());
+            p.instructions.extend(ins);
         }
 
         // log the program
