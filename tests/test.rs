c1f56ae705087d8b262584a5911b3e12fee9a2f2
Peter Richmond <peter.richmond@outlook.com>
2023-05-23 19:48:54 +0100

    update tests

--- tests/test.rs
+++ tests/test.rs
@@ -1,4 +1,4 @@
-use tinyscript::{compile, compile_and_run};
+use tinyscript::{compile_and_run};
 use tinyscript::vm::value::Value;
 
 #[test]
@@ -8,25 +8,30 @@ fn test_hello_world() {
 
 #[test]
 fn test_variables() {
-    assert_eq!(compile_and_run(include_str!("01_variables.tny"), "Variables.main", Value::Array(vec![])).unwrap(), Value::Null);
+    assert_eq!(compile_and_run(include_str!("01_variables.tny"), "Test.main", Value::Array(vec![])).unwrap(), Value::Null);
 }
 
 #[test]
 fn test_ifs() {
-    assert_eq!(compile_and_run(include_str!("02_ifs.tny"), "Ifs.main", Value::Array(vec![])).unwrap(), Value::Null);
+    assert_eq!(compile_and_run(include_str!("02_ifs.tny"), "Test.main", Value::Array(vec![])).unwrap(), Value::Null);
 }
 
 #[test]
-fn test_class_constructor() {
-    assert_eq!(compile_and_run(include_str!("04_class_constructor.tny"), "Ifs.main", Value::Array(vec![])).unwrap(), Value::Null);
+fn test_class() {
+    assert_eq!(compile_and_run(include_str!("04_class.tny"), "Test.main", Value::Array(vec![])).unwrap(), Value::Null);
+}
+
+#[test]
+fn test_this() {
+    assert_eq!(compile_and_run(include_str!("05_this.tny"), "Test.main", Value::Array(vec![])).unwrap(), Value::Null);
 }
 
 #[test]
 fn test_loops() {
-    assert_eq!(compile_and_run(include_str!("03_loops.tny"), "LoopTest.main", Value::Array(vec![])).unwrap(), Value::Null);
+    assert_eq!(compile_and_run(include_str!("03_loops.tny"), "Test.main", Value::Array(vec![])).unwrap(), Value::Null);
 }
 
 #[test]
 fn test_fibonacci() {
-    assert_eq!(compile_and_run(include_str!("21_fib.tny"), "Fibonacci.main", Value::Array(vec![])).unwrap(), Value::Null);
+    assert_eq!(compile_and_run(include_str!("21_fib.tny"), "Test.main", Value::Array(vec![])).unwrap(), Value::Null);
 }
